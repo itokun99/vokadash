040941a7e3b2c8f25f88afb052d85a7c478ebed0
itokun99 <indrawanlisantopersonal@gmail.com>
2024-09-01 11:16:18 +0700

    setup libs/shadcn ui

---
+++ src/entries/app.tsx
@@ -0,0 +1,10 @@
+import "../core/assets/styles/index.css";
+import { Login } from "@/features/auth/views/Login";
+
+export const MatadorDash = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { runMatadorDash } from "./entries";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// run react web app
+runMatadorDash();
+console.log("APP_START");
