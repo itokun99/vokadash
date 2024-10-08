257914c98b3fccbfa004d658cff8400c112a01f1
itokun99 <indrawanlisantopersonal@gmail.com>
2024-10-08 19:13:52 +0700

    update package

--- src/features/_global/components/icon/index.tsx
+++ src/features/_global/components/icon/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as icons from "lucide-react";
 import * as radixIcons from "@radix-ui/react-icons";
 
-type IconNames = keyof typeof icons & keyof typeof radixIcons;
+type IconNames = keyof typeof icons;
 
 export interface IconProps {
   iconName?: IconNames;
@@ -15,11 +15,15 @@ export const Icon = React.memo((props: IconProps) => {
   const isEmpty = !props.iconName && !props.lucideIcon && !props.radixIcon;
   let Component = null;
 
-  if (!isEmpty) return null;
+  if (isEmpty) return null;
 
-  Component =
-    (props.iconName && (icons[props.iconName] || radixIcons[props.iconName])) ||
-    (((props.lucideIcon && icons[props.lucideIcon]) ||
-      (props.radixIcon && radixIcons[props.radixIcon])) as JSX.ElementType);
+  if (props.iconName && icons[props.iconName]) {
+    Component = icons[props.iconName] as JSX.ElementType;
+  } else if (props.lucideIcon && icons[props.lucideIcon]) {
+    Component = icons[props.lucideIcon] as JSX.ElementType;
+  } else if (props.radixIcon && radixIcons[props.radixIcon]) {
+    Component = radixIcons[props.radixIcon] as JSX.ElementType;
+  }
+
   return Component ? <Component {...props} /> : null;
 });
