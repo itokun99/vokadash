6e7edb8be20b19286cc7a5af9cdb0ea4ff9094e0
itokun99 <indrawanlisantopersonal@gmail.com>
2024-11-06 05:25:08 +0700

    update add helmet

--- src/features/_global/components/dashboard/DashboardPageLayout.tsx
+++ src/features/_global/components/dashboard/DashboardPageLayout.tsx
@@ -1,29 +1,44 @@
 import React, { PropsWithChildren } from "react";
 import { CustomBreadcrumbs, CustomBreadcrumbsProps } from "../breadcrumbs";
+import { VokadashHead } from "../../libs";
 
 export interface DashboardPageLayoutProps extends PropsWithChildren {
   title?: string;
   breadcrumbs?: CustomBreadcrumbsProps["items"];
+  siteTitle?: string;
 }
 
 export const DashboardPageLayout = React.memo(
-  ({ title, children, breadcrumbs = [] }: DashboardPageLayoutProps) => {
+  ({
+    title,
+    children,
+    breadcrumbs = [],
+    siteTitle,
+  }: DashboardPageLayoutProps) => {
     return (
-      <div className="dashboard-page-layout flex flex-1 flex-col">
-        {breadcrumbs?.length > 0 && (
-          <div>
-            <CustomBreadcrumbs items={breadcrumbs} />
-          </div>
+      <>
+        {siteTitle && (
+          <VokadashHead>
+            <title>{siteTitle}</title>
+          </VokadashHead>
         )}
-        {title && (
-          <div className="flex items-center">
-            <h1 className="dashboard-page-title  text-lg font-semibold md:text-2xl">
-              {title}
-            </h1>
-          </div>
-        )}
-        {children}
-      </div>
+
+        <div className="dashboard-page-layout flex flex-1 flex-col">
+          {breadcrumbs?.length > 0 && (
+            <div>
+              <CustomBreadcrumbs items={breadcrumbs} />
+            </div>
+          )}
+          {title && (
+            <div className="flex items-center">
+              <h1 className="dashboard-page-title  text-lg font-semibold md:text-2xl">
+                {title}
+              </h1>
+            </div>
+          )}
+          {children}
+        </div>
+      </>
     );
   },
 );
--- src/features/auth/components/AuthLayout.tsx
+++ src/features/auth/components/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import { IMAGES } from "@/core/assets";
+import { VokadashHead } from "@/features/_global";
 
 const bg =
   "https://images.unsplash.com/photo-1490642914619-7955a3fd483c?q=80&w=2093&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
@@ -9,6 +10,7 @@ export interface AuthLayoutProps extends PropsWithChildren {
   description?: string;
   image?: string;
   logo?: string;
+  siteTitle?: string;
 }
 
 export const AuthLayout = ({
@@ -17,51 +19,59 @@ export const AuthLayout = ({
   children,
   image,
   logo,
+  siteTitle,
 }: AuthLayoutProps) => {
   return (
-    <div className="relative w-full lg:grid lg:grid-cols-2 min-h-[100svh]">
-      <div className="sm:hidden">
-        <img
-          src={IMAGES.imgWave}
-          alt=""
-          className="absolute  bottom-0 flex justify-end align-bottom  min-w-full"
-          style={{
-            height: "60vh",
-            objectFit: "cover",
-          }}
-        />
-      </div>
-      <div className="relative z-10 flex min-h-[100svh] items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6 px-4">
-          {logo && (
-            <div className="w-full flex-1">
-              <img className="w-full" src={logo} />
-            </div>
-          )}
+    <>
+      {siteTitle && (
+        <VokadashHead>
+          <title>{siteTitle}</title>
+        </VokadashHead>
+      )}
+      <div className="relative w-full lg:grid lg:grid-cols-2 min-h-[100svh]">
+        <div className="sm:hidden">
+          <img
+            src={IMAGES.imgWave}
+            alt=""
+            className="absolute  bottom-0 flex justify-end align-bottom  min-w-full"
+            style={{
+              height: "60vh",
+              objectFit: "cover",
+            }}
+          />
+        </div>
+        <div className="relative z-10 flex min-h-[100svh] items-center justify-center py-12">
+          <div className="mx-auto grid w-[350px] gap-6 px-4">
+            {logo && (
+              <div className="w-full flex-1">
+                <img className="w-full" src={logo} />
+              </div>
+            )}
 
-          {(title || description) && (
-            <div className="grid gap-2 text-center">
-              {title && <h1 className="text-3xl font-bold">{title}</h1>}
-              {description && (
-                <p className="text-balance text-muted-foreground">
-                  {description}
-                </p>
-              )}
-            </div>
-          )}
+            {(title || description) && (
+              <div className="grid gap-2 text-center">
+                {title && <h1 className="text-3xl font-bold">{title}</h1>}
+                {description && (
+                  <p className="text-balance text-muted-foreground">
+                    {description}
+                  </p>
+                )}
+              </div>
+            )}
 
-          {children}
+            {children}
+          </div>
+        </div>
+        <div className="hidden bg-muted lg:block">
+          <img
+            src={image || bg}
+            alt="Image"
+            width="1920"
+            height="1080"
+            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          />
         </div>
       </div>
-      <div className="hidden bg-muted lg:block">
-        <img
-          src={image || bg}
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
-    </div>
+    </>
   );
 };
