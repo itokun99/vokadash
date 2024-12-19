d351bc231e385899a804131c93f9137712b2c94c
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-19 09:42:45 +0700

    add vokatable

---
+++ src/features/_global/components/table/index.ts
@@ -0,0 +1,1 @@
+export * from "./voka-data-table";
--- src/features/_global/utils/formatter.ts
+++ src/features/_global/utils/formatter.ts
@@ -16,3 +16,32 @@ export const jsonHelper = {
     }
   },
 };
+
+export function searchParamsToObject(
+  searchParams: string,
+): Record<string, string> {
+  const params = new URLSearchParams(searchParams);
+  const obj: Record<string, string> = {};
+  for (const [key, value] of params) {
+    obj[key] = value;
+  }
+  return obj;
+}
+
+export type ParamType = Record<string, string | number | undefined | null>;
+
+export function objectToSearchParams(obj?: ParamType): string {
+  if (!obj || typeof obj !== "object" || Array.isArray(obj)) {
+    return "";
+  }
+
+  const params = new URLSearchParams();
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key];
+    if (typeof value === "string" || typeof value === "number") {
+      params.append(key, value.toString());
+    }
+  });
+
+  return params.toString();
+}
