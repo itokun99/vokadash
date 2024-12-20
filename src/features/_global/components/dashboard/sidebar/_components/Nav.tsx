ca5aed3eb79b74643729c510c11913fea33f400a
itokun99 <indrawanlisantopersonal@gmail.com>
2024-12-20 09:43:55 +0700

    update sidebar

--- src/features/_global/components/dashboard/sidebar/_components/Nav.tsx
+++ src/features/_global/components/dashboard/sidebar/_components/Nav.tsx
@@ -1,15 +1,9 @@
 import { cn } from "@/features/_global/libs/shadcn/lib/utils";
 import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/features/_global/libs/shadcn/components/ui/accordion";
 import { NavItemProps, NavProps } from "../types";
 import { Icon, SidebarContext } from "@/features/_global";
-import { ArrowDown, ArrowUp, ChevronDown, ChevronRight } from "lucide-react";
+import { ChevronDown, ChevronRight } from "lucide-react";
 
 const NavItem = React.memo((props: NavItemProps) => {
   const [visibleChild, setVisibleChild] = useState(false);
@@ -58,70 +52,6 @@ const NavItem = React.memo((props: NavItemProps) => {
       )}
     </li>
   );
-
-  // return (
-  //   <AccordionItem
-  //     value={String(props.value)}
-  //     className="sidebar-nav-accordion border-none"
-  //   >
-  //     <AccordionTrigger className="sidebar-nav-accordion-trigger hover:no-underline py-2 border-none hover:bg-theme-color-info rounded-lg mb-4 px-3">
-  //       <div className="flex gap-3">
-  //         {props.icon && (
-  //           <Icon
-  //             iconName={props.icon}
-  //             className="sidebar-nav-item-icon h-4 w-4"
-  //           />
-  //         )}
-  //         {props.title}
-  //       </div>
-  //     </AccordionTrigger>
-  //     {hasChild && (
-  //       <AccordionContent className="sidebar-nav-submenu bg-mute pl-4">
-  //         {props.items?.map((item, i) => {
-  //           const hasSubChild = item.items && item.items?.length > 0;
-  //           if (hasSubChild) {
-  //             return (
-  //               <Accordion
-  //                 type="single"
-  //                 collapsible
-  //                 className="sidebar-nav grid w-full items-start text-sm font-medium sm:px-4"
-  //                 defaultValue={`${props.value}-${i}-0`}
-  //               >
-  //                 {item.items?.map((d, j) => {
-  //                   return (
-  //                     <NavItem
-  //                       {...d}
-  //                       key={`${props.value}-${i}-${j}`}
-  //                       value={`${props.value}-${i}-${j}`}
-  //                       mobile={props.mobile}
-  //                     />
-  //                   );
-  //                 })}
-  //               </Accordion>
-  //             );
-  //           }
-  //
-  //           return (
-  //             <NavLink
-  //               key={i}
-  //               onClick={sidebarContext.setVisible}
-  //               to={item.url || "#"}
-  //               className={(p) =>
-  //                 cn(
-  //                   "sidebar-nav-item flex items-center px-3 py-2 mb-2 rounded-lg hover:bg-theme-color-info duration-200 gap-4",
-  //                   p.isActive && "bg-theme-color-info",
-  //                 )
-  //               }
-  //             >
-  //               {item.icon && <Icon iconName={item.icon} className="h-4 w-4" />}
-  //               {item.title}
-  //             </NavLink>
-  //           );
-  //         })}
-  //       </AccordionContent>
-  //     )}
-  //   </AccordionItem>
-  // );
 });
 
 export const Nav = React.memo(
@@ -133,18 +63,6 @@ export const Nav = React.memo(
         })}
       </ul>
     );
-    // return (
-    //   <Accordion
-    //     type="single"
-    //     collapsible
-    //     className="sidebar-nav grid w-full items-start text-sm font-medium sm:px-4"
-    //     defaultValue="0"
-    //   >
-    //     {items?.map((item, i) => {
-    //       return <NavItem {...item} key={i} value={i} mobile={mobile} />;
-    //     })}
-    //   </Accordion>
-    // );
   },
 );
 
